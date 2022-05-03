6849ea588f1bf1d92bba4676799ede0363cd2ce9
mario <mario.hirkic@posteo.de>
2022-05-03 12:46:20 +0200

    Zwischenspeichern

--- time_measurement.h
+++ time_measurement.h
@@ -1,26 +1,38 @@
-#include<time.h>
+#include <sys/time.h>   //Time library 
+#include <wiringPi.h>   //Gpio ports 
+
 
 class TimeMeasurement {
     private:
         //CPU time
         struct timespec cpu_time;
         //Time delay on the communication
-        unsigned int time_delay_comm;
+        long long time_delay_comm;
         //Time delay to master cpu
-        unsigned int time_delay_master; 
+        long long time_delay_master; 
         //General indicator
         bool initialized = false; 
         //Measurment
         typedef struct{
             struct timespec start;
             struct timespec end; 
-            unsigned int result;
+            unsigned int elapsed;
+            bool    ready_to_calc;
             bool    done;
-        }measurment; 
+        }measurement; 
+
+        measurement values;
+
+        //Interrupt for sensor measurement
+        void measurementinterrupt();
 
     public: 
+
+        //Initializes all variables and ports
+        void init();
+
         //Calculates time delay to master
-        void timesync_master(unsigned int time_master);
+        void timesync_master(unsigned int time_master); //whsl egal?
 
         //Calculates time delay on bus 
         void timesync_communication(unsigned int measured_delay);
