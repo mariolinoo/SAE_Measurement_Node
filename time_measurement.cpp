04dd463c9310f1cf9a626c7349314a2e6c7054cf
mario <mario.hirkic@posteo.de>
2022-05-03 12:46:02 +0200

    Zwischenspeichern

--- time_measurement.cpp
+++ time_measurement.cpp
@@ -1,23 +1,61 @@
 #include "time_measurement.h"
-
 #include <QtGlobal>
 
-void TimeMeasurement::timesync_master(unsigned int time_master)
+using namespace std;
+
+void TimeMeasurement::init()
 {
-    //Welche Zeit soll dafür genommen werden -> zu welchen Zeitpunkt soll die Zeit lokal festgestellt werden?
-    Q_UNUSED(time_master);
+    //Init GPIO 
+    wiringPiSetup();
+    pinMode(0, INPUT);
+
+    //Time GPIO
+    this->values.start.tv_sec  = 0;
+    this->values.start.tv_nsec = 0;
+    this->values.end.tv_sec  = 0;
+    this->values.end.tv_nsec = 0;
+
+    this->values.ready_to_calc = FALSE;
+    this->values.done = FALSE;
+
+    this->time_delay_comm   = 0;
+    this->time_delay_master = 0;
+
+    this->initialized = TRUE;
+}
+
+void TimeMeasurement::timesync_master(timespec time_master)
+{
+    clock_gettime(CLOCK_REALTIME, &cpu_time);
+    this->time_delay_master = ((cpu_time.tv_sec * 1000000000L) + cpu_time.tv_nsec) - ((time_master.tv_sec * 1000000000L) + time_master.tv_nsec);
 
     return;
 }
 
-void TimeMeasurement::timesync_communication(unsigned int measured_delay)
+void TimeMeasurement::timesync_communication(long long measured_delay)
 {
-    time_delay_comm = measured_delay;
+    this->time_delay_comm = measured_delay;
 
     return;
 }
 
 unsigned int TimeMeasurement::measurement()
 {
+    long long tmp; 
+    clock_gettime(CLOCK_REALTIME, &cpu_time);
+    this->value.start = cpu_time; 
+    wiringPiISR(0, INT_EDGE_RISING, this->measurementinterrupt)
+    while(this->value.ready_to_calc != TRUE);
+    
+    //Measured time calc - prototype
+    tmp = (((this->values.start.tv_sec * 1000000000L) + this->values.start.tv_nsec) - this->time_delay_comm);
+
     return 0;
+}
+
+void Timemeasurement::measurementinterrupt()
+{
+    clock_gettime(CLOCK_REALTIME, &cpu_time);
+    this->values.end = cpu_time;
+    this->values.ready_to_calc = TRUE;
 }
